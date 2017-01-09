cf2e4cca40801a3ea9602d267e108a25e111ec2e
oozkaraca <oguz.ozkaraca@eteration.com>
2017-01-09 17:26:57 +0300

    directive usage

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 function MyController() {
     var vm = this;
     vm.name = 'oguz';
+
+    vm.records = [{
+        name: "Alfreds Futterkiste",
+        city: "Berlin",
+        country: "Germany"
+    }, {
+        name: "Ana Trujillo Emparedados y helados",
+        city: "México D.F.",
+        country: "Mexico"
+    }, {
+        name: "Blondel père et fils",
+        city: "Strasbourg",
+        country: "France"
+    }, {
+        name: "Bólido Comidas preparadas",
+        city: "Madrid",
+        country: "Spain"
+    }];
 }
 
 function MyDirective() {
@@ -12,5 +30,5 @@ function MyDirective() {
     }
 }
 
-angular.module('mydirective', [])
-    .directive('mydirective', MyDirective);
+angular.module('app', [])
+    .directive('myDirective', MyDirective);
